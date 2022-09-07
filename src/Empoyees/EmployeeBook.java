19d9c52c5620b88df9b49fa6f3eaf154501f2bdd
nadillustrator <nadin2208@gmail.com>
2022-09-07 13:57:44 +0300

    Course work 1

--- src/Empoyees/EmployeeBook.java
+++ src/Empoyees/EmployeeBook.java
@@ -25,8 +25,8 @@ public class EmployeeBook {
     public double findSumOfSalaryAllEmployees(){
         double sum = 0;
         for (int i = 0; i < size; i++) {
-            Employee employee = employees[i];
-            sum = sum + employee.getSalary();
+            //Employee employee = employees[i];
+            sum = sum + employees[i].getSalary();
         }
         return sum;
     }
